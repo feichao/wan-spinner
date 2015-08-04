e0a84dcd5148e8dacf4dd2153f8758460d44f3b2
FeichaoWan <len.may@foxmail.com>
2015-08-04 16:44:48 +0800

    bugfix

--- build/wan-spinner.js
+++ build/wan-spinner.js
@@ -29,11 +29,14 @@
     this.options = $.extend({}, this.defaults, options);
     this.options.stepFloat = 1 / this.options.step;
     this.element = $(element);
-    this.options.element = this.element;
-    this.inputElement = $(this.element.children('input'));
 
-    var initValue = this.inputElement.val() || this.options.start;
-    this.inputElement.val(initValue).css('width', this.options.inputWidth);
+    this.element.each(function(index, dt) {
+      var input = $(dt).children('input');
+      var initValue = input.val() || this.options.start;
+      input.val(initValue);
+    });
+
+    this.element.children('input').css('width', this.options.inputWidth);
 
     this.EXCEPTION = {
       TOO_LARGE: 1,
@@ -46,38 +49,40 @@
     var self = this;
     $(self.element).delegate('.minus', 'click', function() {
       var val;
+      var input = $(this).siblings('input');
       if (self.options.stepFloat > 1) {
-        val = (+self.inputElement.val() || 0) * self.options.stepFloat - (self.options.step || 1) * self.options.stepFloat;
+        val = (+input.val() || 0) * self.options.stepFloat - (self.options.step || 1) * self.options.stepFloat;
         val = Math.round(val) / self.options.stepFloat;
       } else {
-        val = (+self.inputElement.val() || 0) - (self.options.step || 1);
+        val = (+input.val() || 0) - (self.options.step || 1);
       }
       if (val < self.options.minValue) {
         typeof(self.options.exceptionFun) === 'function' && self.options.exceptionFun(self.EXCEPTION.TOO_SMALL);
       } else {
-        self.inputElement.val(val);
-        typeof(self.options.minusClick) === 'function' && self.options.minusClick(self.options.element, val);
-        typeof(self.options.valueChanged) === 'function' && self.options.valueChanged(self.options.element, val);
+        input.val(val);
+        typeof(self.options.minusClick) === 'function' && self.options.minusClick($(this).parent(), val);
+        typeof(self.options.valueChanged) === 'function' && self.options.valueChanged($(this).parent(), val);
       }
       return false;
     }).delegate('.plus', 'click', function() {
       var val;
+      var input = $(this).siblings('input');
       if (self.options.stepFloat > 1) {
-        val = (+self.inputElement.val() || 0) * self.options.stepFloat + (self.options.step || 1) * self.options.stepFloat;
+        val = (+input.val() || 0) * self.options.stepFloat + (self.options.step || 1) * self.options.stepFloat;
         val = Math.round(val) / self.options.stepFloat;
       } else {
-        val = (+self.inputElement.val() || 0) + (self.options.step || 1);
+        val = (+input.val() || 0) + (self.options.step || 1);
       }
       if (val > self.options.maxValue) {
         typeof(self.options.exceptionFun) === 'function' && self.options.exceptionFun(self.EXCEPTION.TOO_LARGE);
       } else {
-        self.inputElement.val(val);
-        typeof(self.options.plusClick) === 'function' && self.options.plusClick(self.options.element, val);
-        typeof(self.options.valueChanged) === 'function' && self.options.valueChanged(self.options.element, val);
+        input.val(val);
+        typeof(self.options.plusClick) === 'function' && self.options.plusClick($(this).parent(), val);
+        typeof(self.options.valueChanged) === 'function' && self.options.valueChanged($(this).parent(), val);
       }
       return false;
     }).delegate('input', 'change', function() {
-      var val = +self.inputElement.val() || 0;
+      var val = +$(this).val() || 0;
       if (val > self.options.maxValue) {
         val = self.options.maxValue;
         typeof(self.options.exceptionFun) === 'function' && self.options.exceptionFun(self.EXCEPTION.TOO_LARGE);
@@ -85,8 +90,8 @@
         val = self.options.minValue;
         typeof(self.options.exceptionFun) === 'function' && self.options.exceptionFun(self.EXCEPTION.TOO_SMALL);
       }
-      self.inputElement.val(val);
-      typeof(self.options.valueChanged) === 'function' && self.options.valueChanged(self.options.element, val);
+      $(this).val(val);
+      typeof(self.options.valueChanged) === 'function' && self.options.valueChanged($(this).parent(), val);
     });
   }
 
